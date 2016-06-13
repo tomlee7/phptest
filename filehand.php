9419dc9f66278fdac1325a7c3914d297d4b3d532
tomlee7 <tamoghna.maitra@gmail.com>
2016-06-13 22:26:57 +0530

    Adding files

---
+++ filehand.php
@@ -0,0 +1,12 @@
+<?php
+$File = "YourFile.txt";
+$Handle = fopen($File, 'w');
+$Data = $_POST['name']."<br/>";
+fwrite($Handle, $Data); 
+$Data = $_POST['email']."<br/>";
+fwrite($Handle, $Data); 
+$Data=$_POST['comments']."<br/>";
+fwrite($Handle,$Data);
+print "Data Written";
+fclose($Handle); 
+?>
---
+++ process.php
@@ -0,0 +1,6 @@
+<?php
+    echo $_POST['name']."<br/>";
+    echo $_POST['email']."<br/>";
+    echo $_POST['comments']."<br/>";
+
+?>
